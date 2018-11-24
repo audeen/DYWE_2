ca030b095119b8a9693d4abf71cb8726103206e5
audeen <jannik.sievert@gmail.com>
2018-11-24 11:13:31 +0100

    ..

--- V1/intern/sites/login.php
+++ V1/intern/sites/login.php
@@ -11,34 +11,10 @@
 session_start();
 
 include ("../../config/config.php");
+echo "<link rel=\"stylesheet\"  type=\"text/css\" href=\"../../config/css/style.css\">"; 
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-     // username and password sent from form
-    $myusername = mysqli_real_escape_string($db,$_POST['user']);
-    $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-
-    $sql = "SELECT u_id FROM user WHERE user = '$myusername' and password = '$mypassword'";
-    $result = mysqli_query($db,$sql);
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-    $active = $row['active'];
-
-    $count = mysqli_num_rows($result);
-    // If result matched $myusername and $mypassword, table row must be 1 row
-
-    if($count == 1) {
-        session_register("myusername");
-        $_SESSION['login_user'] = $myusername;
-
-        header("location: liste.php");
-    }
-    else {
-        $error = "Eingabe falsch";
-    }
-}
-
-
-
+/* if (!isset )
+ */
 
 echo "<html>";
 echo "<head>";
